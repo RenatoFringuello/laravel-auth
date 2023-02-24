0a8995066a31f2cea4151aaafced3aa3b44c9678
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-24 23:18:03 +0100

    add img in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,13 +3,14 @@
 @section('title', "Manage project n°$project->id | RF")
 
 @section('content')
-        <div class="col-12 col-md-8 mt-5 mx-auto text-decoration-none text-black">
+        <div class="col-12 col-md-6 mt-5 mx-auto text-decoration-none text-black">
             <div class="card p-2">
+                <img src="{{ asset('storage/' . $project->image) }}" class="img-fluid rounded-1 mb-3" alt="{{ $project->image }}">
                 <h2>{{ $project->title }}</h2>
-                <pre class="text-secondary fs-5">{{ $project->author_name . ' ' . $project->author_lastname }}</pre>
-                <p>{{$project->content}}</p>
+                <pre class="text-secondary fs-5 mb-3">{{ $project->author_name . ' ' . $project->author_lastname }}</pre>
+                <p class="mb-3">{{$project->content}}</p>
                 <div>{{ $project->start_date->format('Y-m-d') }}</div>
-                <div class="mb-2 text-success {{ $project->end_date ?? 'text-danger' }}">{{ isset($project->end_date) ? $project->end_date->format('Y-m-d'): 'work in progress' }}</div>
+                <div class="mb-3 text-success {{ $project->end_date ?? 'text-danger' }}">{{ isset($project->end_date) ? $project->end_date->format('Y-m-d'): 'work in progress' }}</div>
                 <div class="btn-container">
                     <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary">
                         <i class="fa-solid fa-pen"></i>
