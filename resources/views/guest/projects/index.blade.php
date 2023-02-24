635458ad91977614597a2771d7b6f536c7123244
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-24 16:13:24 +0100

    add guest pages

---
+++ resources/views/guest/projects/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', count($projects). ' projects | RF')
+
+@section('content')
+    {{-- homepage --}}
+    {{ $projects->links()}}
+    <div class="row g-3 mb-3">
+        @foreach ($projects as $project)
+            <a href="{{route('guest.projects.show', $project)}}" class="col-12 col-sm-6 col-lg-4 col-xl-3 text-decoration-none text-black">
+                <div class="card p-2">
+                    <h4>{{ $project->title }}</h4>
+                    <pre class="text-secondary">{{ $project->author_name . ' ' . $project->author_lastname }}</pre>
+                    <p>{{ $project->content }}</p>
+                    <div>{{ $project->start_date->format('Y-m-d') }}</div>
+                    <div class="text-success {{ $project->end_date ?? 'text-danger' }}">{{ isset($project->end_date) ? $project->end_date->format('Y-m-d'): 'work in progress' }}</div>
+                </div>
+            </a>
+        @endforeach     
+    </div>
+    {{ $projects->links()}}
+@endsection
