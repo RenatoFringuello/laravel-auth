66db267a0b79abf9240419c3cf88da4a2706586d
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-25 21:07:42 +0100

    add a message if there are not project to show

--- resources/views/guest/projects/index.blade.php
+++ resources/views/guest/projects/index.blade.php
@@ -9,24 +9,32 @@
             {{ $projects->links()}}
         </div>
         <div class="row g-3 mb-3">
-            @foreach ($projects as $project)
-            <a  href="{{route('guest.projects.show', $project)}}" 
-                class="col-12 col-sm-6 col-lg-4 col-xl-3 text-decoration-none text-black">
+            @forelse ($projects as $project)
+                <a  href="{{route('guest.projects.show', $project)}}" 
+                    class="col-12 col-sm-6 col-lg-4 col-xl-3 text-decoration-none text-black">
 
-                <div class="card p-2 h-100 d-flex flex-column justify-content-between">
-                    <div class="top">
-                        <h4>{{ $project->title }}</h4>
-                        <pre class="text-secondary">{{ $project->author_name . ' ' . $project->author_lastname }}</pre>
-                        <p>{{ $project->content }}</p>
+                    <div class="card p-2 h-100 d-flex flex-column justify-content-between">
+                        <div class="top">
+                            <h4>{{ $project->title }}</h4>
+                            <pre class="text-secondary">{{ $project->author_name . ' ' . $project->author_lastname }}</pre>
+                            <p>{{ $project->content }}</p>
+                        </div>
+                        <div class="bottom">
+                            <div>{{ $project->start_date->format('Y-m-d') }}</div>
+                            <div class="text-success {{ $project->end_date ?? 'text-danger' }}">{{ isset($project->end_date) ? $project->end_date->format('Y-m-d'): 'work in progress' }}</div>
+                        </div>
                     </div>
-                    <div class="bottom">
-                        <div>{{ $project->start_date->format('Y-m-d') }}</div>
-                        <div class="text-success {{ $project->end_date ?? 'text-danger' }}">{{ isset($project->end_date) ? $project->end_date->format('Y-m-d'): 'work in progress' }}</div>
-                    </div>
+                
+                </a>
+
+                @empty
+
+                <div class="col-12 col-sm-6 col-lg-4 col-xl-3 w-100 pt-5 text-secondary">
+                    <h1 class="text-center pt-5">
+                        Non ci sono progetti da visualizzare
+                    </h1>
                 </div>
-            
-            </a>
-            @endforeach     
+            @endforelse
         </div>
         {{ $projects->links()}}
     </div>
