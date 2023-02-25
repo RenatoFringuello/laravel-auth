759c2828f60c2b8b764d67f1f096fd5dcae30316
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-25 20:17:49 +0100

    add card in show style

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -9,23 +9,31 @@
                 <div class="left w-md-50 mb-3 mb-md-0">
                     <img src="{{ asset('storage/'.$project->image) }}" class="img-fluid h-100 object-fit-cover object-position-center rounded-1" alt="{{ $project->image }}">
                 </div>
-                <div class="right w-md-50 px-md-3">
-                    <h2>{{ $project->title }}</h2>
-                    <pre class="text-secondary fs-5 mb-3">{{ $project->author_name . ' ' . $project->author_lastname }}</pre>
-                    <p class="mb-3">{{$project->content}}</p>
-                    <div>{{ $project->start_date->format('Y-m-d') }}</div>
-                    <div class="mb-3 text-success {{ $project->end_date ?? 'text-danger' }}">{{ isset($project->end_date) ? $project->end_date->format('Y-m-d'): 'work in progress' }}</div>
-                    <div class="btn-container">
-                        <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary">
-                            <i class="fa-solid fa-pen"></i>
-                        </a>
-                        @include('layouts.partials.form', ['method' => 'DELETE', 'route' => 'admin.projects.destroy', 'orderBy'=> null, 'project' => $project, 'extraClasses' => 'btn p-0'])
+                <div class="right w-100 w-md-50 ps-md-4 p-md-3 d-flex flex-column justify-content-between">
+                    <div class="top">
+                        <h2>{{ $project->title }}</h2>
+                        <pre class="text-secondary fs-5 mb-3">{{ $project->author_name . ' ' . $project->author_lastname }}</pre>
+                        <p class="mb-3">{{$project->content}}</p>
+                    </div>
+                    <div class="bottom d-flex align-items-center justify-content-between">
+                        <div class="dates-container">
+                            <div>{{ $project->start_date->format('Y-m-d') }}</div>
+                            <div class="text-success {{ $project->end_date ?? 'text-danger' }}">{{ isset($project->end_date) ? $project->end_date->format('Y-m-d'): 'work in progress' }}</div>
+                        </div>
+                        <div class="btn-container">
+                            <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary">
+                                <i class="fa-solid fa-pen"></i>
+                            </a>
+                            @include('layouts.partials.form', ['method' => 'DELETE', 'route' => 'admin.projects.destroy', 'orderBy'=> null, 'project' => $project, 'extraClasses' => 'btn p-0'])
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
+
 
 @section('scripts')
     <script type='module' src="{{Vite::asset('./resources/js/popUp.js')}}"></script>
