534f007d214d3c74be3ed8fb9ce9f4548e325ba1
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-26 17:40:10 +0100

    add relation one to many (reverse)

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,16 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['slug','title', 'author_username', 'author_name', 'author_lastname', 'content', 'start_date', 'end_date', 'image'];
+    // 'author_username', 'author_name', 'author_lastname',
+    protected $fillable = ['slug','title',  'content', 'start_date', 'end_date', 'image', 'user_id'];
     protected $dates = ['start_date','end_date'];
 
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
     }
 }
