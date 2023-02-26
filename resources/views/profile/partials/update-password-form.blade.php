bcef5da38fa53870be9e10a26fbfc892b164f850
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-26 12:34:40 +0100

    move success log and margins

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -7,13 +7,25 @@
         <p class="mt-1 text-sm text-gray-600">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
+
+        @if (session('status') === 'password-updated')
+            <script>
+                const show = true;
+                setTimeout(() => show = false, 2000)
+                const el = document.getElementById('status')
+                if (show) {
+                    el.style.display = 'block';
+                }
+            </script>
+            <p id='status' class="alert alert-success">{{ __('Saved.') }}</p>
+        @endif
     </header>
 
     <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('put')
 
-        <div class="mb-2">
+        <div class="mb-3">
             <label for="current_password">{{__('Current Password')}}</label>
             <input class="mt-1 form-control" type="password" name="current_password" id="current_password" autocomplete="current-password">
             @error('current_password')
@@ -23,7 +35,7 @@
             @enderror
         </div>
 
-        <div class="mb-2">
+        <div class="mb-3">
             <label for="password">{{__('New Password')}}</label>
             <input class="mt-1 form-control" type="password" name="password" id="password" autocomplete="new-password">
             @error('password')
@@ -33,7 +45,7 @@
             @enderror
         </div>
 
-        <div class="mb-2">
+        <div class="mb-3">
 
             <label for="password_confirmation">{{__('Confirm Password')}}</label>
             <input class="mt-2 form-control" type="password" name="password_confirmation" id="password_confirmation" autocomplete="new-password">
@@ -44,20 +56,6 @@
             @enderror
         </div>
 
-        <div class="d-flex align-items-center gap-4">
-            <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
-
-            @if (session('status') === 'password-updated')
-            <script>
-                const show = true;
-                setTimeout(() => show = false, 2000)
-                const el = document.getElementById('status')
-                if (show) {
-                    el.style.display = 'block';
-                }
-            </script>
-            <p id='status' class=" fs-5 text-muted">{{ __('Saved.') }}</p>
-            @endif
-        </div>
+        <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
     </form>
 </section>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -7,6 +7,17 @@
         <p class="mt-1">
             {{ __("Update your account's profile information and email address.") }}
         </p>
+        @if (session('status') === 'profile-updated')
+            <script>
+                const show = true;
+                setTimeout(() => show = false, 2000)
+                const el = document.getElementById('profile-status')
+                if (show) {
+                    el.style.display = 'block';
+                }
+            </script>
+            <p id='profile-status' class="alert alert-success">{{ __('Saved.') }}</p>
+        @endif
     </header>
 
     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
@@ -17,9 +28,9 @@
         @csrf
         @method('patch')
 
-        <div class="mb-2">
+        <div class="mb-3">
             <label for="username">{{__('Username')}}</label>
-            <input class="form-control" type="text" name="username" id="username" autocomplete="username" value="{{old('username', $user->username)}}" required autofocus>
+            <input class="form-control mt-1" type="text" name="username" id="username" autocomplete="username" value="{{old('username', $user->username)}}" required autofocus>
             
             @error('username')
             <div class="alert alert-danger mt-2" role="alert">
@@ -27,18 +38,18 @@
             </div>
             @enderror
         </div>
-        <div class="mb-2">
+        <div class="mb-3">
             <label for="name">{{__('Name')}}</label>
-            <input class="form-control" type="text" name="name" id="name" autocomplete="name" value="{{old('name', $user->name)}}" required autofocus>
+            <input class="form-control mt-1" type="text" name="name" id="name" autocomplete="name" value="{{old('name', $user->name)}}" required autofocus>
             @error('name')
             <div class="alert alert-danger mt-2" role="alert">
                 <strong>{{ $message}}</strong>
             </div>
             @enderror
         </div>
-        <div class="mb-2">
+        <div class="mb-3">
             <label for="lastname">{{__('Lastname')}}</label>
-            <input class="form-control" type="text" name="lastname" id="lastname" autocomplete="lastname" value="{{old('lastname', $user->lastname)}}" required autofocus>
+            <input class="form-control mt-1" type="text" name="lastname" id="lastname" autocomplete="lastname" value="{{old('lastname', $user->lastname)}}" required autofocus>
             @error('lastname')
             <div class="alert alert-danger mt-2" role="alert">
                 <strong>{{ $message}}</strong>
@@ -46,12 +57,12 @@
             @enderror
         </div>
 
-        <div class="mb-2">
+        <div class="mb-3">
             <label for="email">
                 {{__('Email') }}
             </label>
 
-            <input id="email" name="email" type="email" class="form-control" value="{{ old('email', $user->email)}}" required autocomplete="username" />
+            <input id="email" name="email" type="email" class="form-control mt-1" value="{{ old('email', $user->email)}}" required autocomplete="username" />
 
             @error('email')
             <div class="alert alert-danger mt-2" role="alert">
@@ -78,20 +89,6 @@
             @endif
         </div>
 
-        <div class="d-flex align-items-center gap-4">
-            <button class="btn btn-primary" type="submit">{{ __('Save') }}</button>
-
-            @if (session('status') === 'profile-updated')
-            <script>
-                const show = true;
-                setTimeout(() => show = false, 2000)
-                const el = document.getElementById('profile-status')
-                if (show) {
-                    el.style.display = 'block';
-                }
-            </script>
-            <div id='profile-status' class="fs-5 text-success">{{ __('Saved.') }}</div>
-            @endif
-        </div>
+        <button class="btn btn-primary" type="submit">{{ __('Save') }}</button>
     </form>
 </section>
